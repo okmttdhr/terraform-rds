b7b1d1a80bb10289d9ef39bd29a1fc0f6df19010
okmttdhr <10101114fin@gmail.com>
2018-06-20 22:19:39 +0900

    change port

--- modules/security_group/security_group.tf
+++ modules/security_group/security_group.tf
@@ -11,8 +11,8 @@ resource "aws_security_group" "default" {
   vpc_id = "${var.vpc_id}"
 
   ingress {
-    from_port = 22
-    to_port = 22
+    from_port = 3306
+    to_port = 3306
     protocol = "tcp"
     cidr_blocks = ["${var.cidrs}"]
   }
